f7c153df587eb0acd5baef713e7959e37b01895c
William L. Visentini <williamvisentini@gmail.com>
2023-10-02 15:03:23 -0300

    add-authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const crypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -8,17 +9,66 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
+  .then(user => {
+    if (!user){
+      return res.redirect('/login');
+    }
+    crypt.compare(password, user.password)
+    .then(doMatch => {
+      if (doMatch){
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        return req.session.save(err => {
+          console.log(err)
+          res.redirect('/');
+        });
+      }
+      res.redirect('/login');
     })
-    .catch(err => console.log(err));
+    .catch(err =>  {
+      console.log(err);
+      res.redirect('/login');
+    });
+  })
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      return crypt.hash(password, 12)
+      .then(hashedPassword => {
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+          cart: { items: [] }
+        });
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/login');
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
